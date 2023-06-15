328cc8aa22d1740bf2a60f12b0e8e80c5aa81f7a
serenajung <syjylp93@gmail.com>
2023-06-15 15:14:59 +0900

    be first commit

---
+++ server/Pre-Project/src/test/java/com/codestates/PreProject/PreProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codestates.PreProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
