fe5ecbffb91e8edee9ea76e54a46224044d313cb
yelm <21yrshin@naver.com>
2023-06-27 02:58:22 +0900

    shell script added

---
+++ server/Pre-Project/restart.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# if server is still running, then stop it
+ps -ef | grep "Pre-Project-0.0.1-SNAPSHOT.jar" | grep -v grep | awk '{print $2}' | xargs kill -9 2> /dev/null
+
+if [ $? -eq 0 ];then
+    echo "Application Stop Success!"
+else
+    echo "Application Not Running"
+fi
+
+# restart server deployment file
+echo "Application Restart"
+echo $1
+
+# nohup executes server on background
+nohup java -jar build/libs/DeployServer-0.0.1-SNAPSHOT.jar --spring.profiles.active=dev > /dev/null 2>&1 &
