d9f3fc976c9627dbc18959ca0e301684d2a57c41
rinorinorinorino <rinorinorinorino0601@gmail.com>
2023-06-27 11:49:59 +0900

    병합 전 커밋

--- client/src/components/MainPageComponents/Tags.jsx
+++ client/src/components/MainPageComponents/Tags.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 export default function Tags({ tags }) {
   return (
-    <div className=" mr-10">
+    <div className=" flex flex-wrap mr-10">
       {tags.map((tag, idx) => (
-        <span key={idx} className=" text-xs bg-[#E1ECF4] text-[#2e5f80] rounded-[4px] p-2 mr-2 cursor-pointer">
+        <span key={idx} className=" text-xs bg-[#E1ECF4] text-[#2e5f80] rounded-[4px] p-2 mr-2 mb-2 cursor-pointer">
           {tag}
         </span>
       ))}
--- client/src/components/header/LoginButton.jsx
+++ client/src/components/header/LoginButton.jsx
@@ -1,5 +1,5 @@
 // import { useState } from "react"
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
 // function BeforeLogin() {
 //   return (
@@ -49,28 +49,22 @@ function LoginButton() {
     // {!isLogin && <BeforeLogin/>}
     // </>
     <div className="flex ml-[6px] h-[36px]">
-      
       <button
-          type="button"
-          className="bg-[#e1ecf4] hover:bg-[#B3D3EA] text-[#39739d] border-[1px] border-[#b2cbdd] rounded-[4px] px-[10px] w-[70px]"
-        >
-          <Link to= '/login' className="links">
+        type="button"
+        className="bg-[#e1ecf4] hover:bg-[#B3D3EA] text-[#39739d] border-[1px] border-[#b2cbdd] rounded-[4px] px-[10px] w-[70px]"
+      >
+        <Link to="/login" className="links">
           Log in
-          </Link>
-        </button>
-        
-        
-        <button
-          type="button"
-          className="ml-[6px] bg-[#1e95ff] hover:bg-[#0074CC] text-white rounded-[4px] px-[10px] w-[70px]"
-        >
-          <Link to= '/signup' className="links">
+        </Link>
+      </button>
+
+      <button type="button" className="ml-[6px] bg-[#1e95ff] hover:bg-[#0074CC] text-white rounded-[4px] px-[10px] w-[70px]">
+        <Link to="/signup" className="links">
           Sign up
-          </Link>
-        </button>
-        
+        </Link>
+      </button>
     </div>
-  )
+  );
 }
 
-export default LoginButton
+export default LoginButton;
